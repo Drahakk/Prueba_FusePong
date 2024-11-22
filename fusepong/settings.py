7baa5d0f5b71c7ea9377e9e748ce81c98524e50d
Drahakk <148347206+Drahakk@users.noreply.github.com>
2024-11-21 21:10:12 -0500

    Update settings.py

--- fusepong/settings.py
+++ fusepong/settings.py
@@ -12,10 +12,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-import os
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
